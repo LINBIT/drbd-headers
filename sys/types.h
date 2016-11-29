4037b3315200b5ffa781e03087af0df44bd5c64d
Philipp Marek <philipp.marek@linbit.com>
2016-11-29 13:06:52 +0100

    WDRBD: Some header files, mostly empty.

---
+++ sys/types.h
@@ -0,0 +1,11 @@
+#ifndef __WDRBD_SYS_TYPES_H
+#define __WDRBD_SYS_TYPES_H
+
+#include </usr/include/sys/types.h>
+
+
+/* Cygwin types.h doesn't have off64_t. */
+typedef int64_t off64_t;
+
+
+#endif
