e022a20b397847539241fe17d3de93f3987653a0
Philipp Reisner <philipp.reisner@linbit.com>
2017-05-29 16:01:36 +0200

    New configure option on_no_quorum

--- linux/drbd_genl.h
+++ linux/drbd_genl.h
@@ -143,6 +143,7 @@ GENL_struct(DRBD_NLA_RESOURCE_OPTS, 4, res_opts,
 	__u32_field_def(9,	0 /* OPTIONAL */,	auto_promote_timeout, DRBD_AUTO_PROMOTE_TIMEOUT_DEF)
 	__u32_field_def(10,	0 /* OPTIONAL */,	nr_requests, DRBD_NR_REQUESTS_DEF)
 	__s32_field_def(11,	0 /* OPTIONAL */,	quorum, DRBD_QUORUM_DEF)
+	__u32_field_def(12,     0 /* OPTIONAL */,	on_no_quorum, DRBD_ON_NO_QUORUM_DEF)
 )
 
 GENL_struct(DRBD_NLA_NET_CONF, 5, net_conf,
