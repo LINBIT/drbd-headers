f92fa55dcb92a400b2aa7dbc199a924d46725efb
Philipp Reisner <philipp.reisner@linbit.com>
2017-08-17 09:14:49 +0200

    rcu member in struct path
    to allow call_rcu() when destroying a path

--- drbd_transport.h
+++ drbd_transport.h
@@ -11,7 +11,7 @@
    So that transport compiled against an older version of this
    header will no longer load in a module that assumes a newer
    version. */
-#define DRBD_TRANSPORT_API_VERSION 15
+#define DRBD_TRANSPORT_API_VERSION 16
 
 /* MSG_MSG_DONTROUTE and MSG_PROBE are not used by DRBD. I.e.
    we can reuse these flags for our purposes */
@@ -96,6 +96,8 @@ struct drbd_path {
 	struct list_head listener_link; /* paths waiting for an incomming connection,
 					   head is in a drbd_listener */
 	struct drbd_listener *listener;
+
+	struct rcu_head rcu;
 };
 
 /* Each transport implementation should embed a struct drbd_transport
