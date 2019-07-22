f55fd7432ec452e46c338ffa6257be354ec69936
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-07-22 16:34:39 +0200

    compat: follow upstream, make policy common to netlink family
    In Linux kernel commit 3b0f31f2b8c9 ("genetlink: make policy common to family"),
    the "policy" member was moved from struct genl_ops to struct genl_family.

--- linux/genl_magic_func.h
+++ linux/genl_magic_func.h
@@ -233,7 +233,6 @@ static const char *CONCAT_(GENL_MAGIC_FAMILY, _genl_cmd_to_str)(__u8 cmd)
 {								\
 	handler							\
 	.cmd = op_name,						\
-	.policy	= CONCAT_(GENL_MAGIC_FAMILY, _tla_nl_policy),	\
 },
 
 #define ZZZ_genl_ops		CONCAT_(GENL_MAGIC_FAMILY, _genl_ops)
@@ -292,6 +291,7 @@ static struct genl_family ZZZ_genl_family __read_mostly = {
 #ifdef COMPAT_HAVE_GENL_FAMILY_PARALLEL_OPS
 	.parallel_ops = true,
 #endif
+	.policy = CONCAT_(GENL_MAGIC_FAMILY, _tla_nl_policy),
 };
 
 /*
