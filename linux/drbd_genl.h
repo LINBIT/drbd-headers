e970369ffa190cd8d7eebe75e0f14b3b670dc4bf
Joel Colledge <joel.colledge@linbit.com>
2020-11-02 17:05:33 +0100

    Revert "introduce a resource's promotion score"
    This reverts commit 8dea54274ee6e89c0159701e9d39130ce93e4244.
    
    This field was never used. The promotion values are now calculated in
    user space.

--- linux/drbd_genl.h
+++ linux/drbd_genl.h
@@ -230,7 +230,6 @@ GENL_struct(DRBD_NLA_RESOURCE_INFO, 15, resource_info,
 	__flg_field(3, 0, res_susp_nod)
 	__flg_field(4, 0, res_susp_fen)
 	__flg_field(5, 0, res_susp_quorum)
-	__u32_field(6, 0, res_promotion_score)
 )
 
 GENL_struct(DRBD_NLA_DEVICE_INFO, 16, device_info,
