379b0def69f540f842c9ae415cb69e9c4517535e
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2021-03-02 17:01:50 +0100

    add fields for backing dev paths to device/peer_device info

--- linux/drbd_genl.h
+++ linux/drbd_genl.h
@@ -236,6 +236,7 @@ GENL_struct(DRBD_NLA_DEVICE_INFO, 16, device_info,
 	__u32_field(1, 0, dev_disk_state)
 	__flg_field(2, 0, is_intentional_diskless)
 	__flg_field(3, 0, dev_has_quorum)
+	__str_field(4, 0, backing_dev_path, 128)
 )
 
 GENL_struct(DRBD_NLA_CONNECTION_INFO, 17, connection_info,
@@ -250,6 +251,7 @@ GENL_struct(DRBD_NLA_PEER_DEVICE_INFO, 18, peer_device_info,
 	__u32_field(4, 0, peer_resync_susp_peer)
 	__u32_field(5, 0, peer_resync_susp_dependency)
 	__flg_field(6, 0, peer_is_intentional_diskless)
+	__str_field(7, 0, peer_backing_dev_path, 128)
 )
 
 GENL_struct(DRBD_NLA_RESOURCE_STATISTICS, 19, resource_statistics,
