1cdac88155ddafce6ea5290864e6ba48ab80cade
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2022-11-10 15:37:39 +0100

    drbd: add SPDX license identifiers to all files
    We want consistent licenses for the DRBD code base. Downstream,
    GPL-2.0-or-later was guessed for us. Upstream, it's a mix of
    GPL-2.0(-only) and GPL-2.0-or-later.
    
    We have decided that we want GPL-2.0-only, so use that consistently in
    all files.

--- drbd_protocol.h
+++ drbd_protocol.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 #ifndef __DRBD_PROTOCOL_H
 #define __DRBD_PROTOCOL_H
 
--- drbd_transport.h
+++ drbd_transport.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 #ifndef DRBD_TRANSPORT_H
 #define DRBD_TRANSPORT_H
 
